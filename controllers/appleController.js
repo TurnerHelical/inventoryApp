09959dd65bc6b03686b7e28e050ee760abc6d98c
TurnerHelical <hunterde91@gmail.com>
2025-11-20 08:47:02 -0500

    added basic auth for deleting and adding apples

--- controllers/appleController.js
+++ controllers/appleController.js
@@ -82,21 +82,32 @@ async function newAppleGet(req, res) {
 
 async function newApplePost(req, res) {
     try {
-        const apple = {
-            name: `${req.body.name}`,
-            nickname:`${req.body.nickname}`,
-            origin: `${req.body.origin}`,
-            image_link: `${req.body.image_link}`,
-            notes: `${req.body.notes}`,
-            color:`${req.body.color}`,
-            avgPrice:`${req.body.avgPrice}`
-        }
-        await db.newApple(apple);
-        res.redirect('/apples/new?success=1');
+        const secretPass = req.body.adminPassword;
+        if (secretPass != 'superSecretPassword') {
+            res.render('newApple', {
+                title: 'New Apple Form',
+                stylesheet: 'styles/update.css',
+                error: 'Incorrect password, please try again',
+                success: null,
+            });
+        } else {
+            const apple = {
+                name: `${req.body.name}`,
+                nickname:`${req.body.nickname}`,
+                origin: `${req.body.origin}`,
+                image_link: `${req.body.image_link}`,
+                notes: `${req.body.notes}`,
+                color:`${req.body.color}`,
+                avgPrice:`${req.body.avgPrice}`
+            };
+        
+            await db.newApple(apple);
+            res.redirect('/apples/new?success=1');
+        };
     } catch (err) {
         res.status(500).render('error', {
             title:'Server Error',
-            stylesheet:'/styles/error.css',
+            stylesheet:'styles/error.css',
             error: err.message,
             error_code:'500',
         });
@@ -127,17 +138,19 @@ async function updateAppleFormGet(req, res) {
 async function updateAppleFormPost(req, res) {
     try {
         const apple = {
-            id: `${req.params.id}`,
-            name: `${req.body.name}`,
-            nickname:`${req.body.nickname}`,
-            origin: `${req.body.origin}`,
-            image_link: `${req.body.image_link}`,
-            notes: `${req.body.notes}`,
-            color:`${req.body.color}`,
-            avgPrice:`${req.body.avgPrice}`
-        };
-        await db.updateApple(apple);
-        res.redirect(`/apples/${req.params.id}?success=1`);
+                id: `${req.params.id}`,
+                name: `${req.body.name}`,
+                nickname:`${req.body.nickname}`,
+                origin: `${req.body.origin}`,
+                image_link: `${req.body.image_link}`,
+                notes: `${req.body.notes}`,
+                color:`${req.body.color}`,
+                avgPrice:`${req.body.avgPrice}`
+            };
+
+                    
+            await db.updateApple(apple);
+            res.redirect(`/apples/${req.params.id}?success=1`);
     } catch (err) {
         res.status(400).render('error', {
             title: 'Bad Request',
@@ -150,8 +163,9 @@ async function updateAppleFormPost(req, res) {
 
 async function deleteApple(req, res) {
     try {
-        await db.deleteApple(req.params.id);
-        res.redirect('/apples');
+            await db.deleteApple(req.params.id);
+            res.redirect('/apples');
+        
     } catch (err) {
         res.status(500).render('error', {
             title: 'Server Error',
